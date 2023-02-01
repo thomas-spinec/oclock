feed56a4d88009ffec6686b2a89f349e52fc7c25
thomas-spinec <thomas.spinec@laplateforme.io>
2023-02-01 15:19:27 +0100

    minuteur fait

---
+++ minuteur.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minuteur</title>
+    <link rel="icon" type="images/png" sizes="64x64" href="img/Logo_onglet.png" />
+    <link rel="stylesheet" href="style.css">
+    <script src="script/minuteur.js"></script>
+</head>
+
+<body>
+    <header>
+        <section class="centrage" id="header">
+            <h1><a href="index.php">O-clock</a></h1>
+            <p>L'horloge multifonction à portée de souris!</p>
+        </section>
+    </header>
+
+    <main>
+        <section class="centrage" id="main">
+            <h1>Minuteur</h1>
+            <div class="popup">
+                <span id="popup-p"></span>
+                <button type="button" id="close-popup">OK</button>
+            </div>
+            <div id="minuteur">
+                <form action="" method="post">
+                    <div id="numbersTimer">
+                        <input id="hours" name="hours" value="00" type="number" />
+                        <p>:</p>
+                        <input id="minutes" name="minutes" value="00" type="number" />
+                        <p>:</p>
+                        <input id="seconds" name="seconds" value="00" type="number" />
+                    </div>
+                    <button id="buttonMinuteur" type="submit" name="submitMinuteur" data-switch="on">on</button>
+                </form>
+                <button id="reset">Reset</button>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <section class="centrage">
+            <img id="perso" src="img/Logo.png" alt="Thomas Spinec">
+        </section>
+    </footer>
+</body>
+
+</html>
---
+++ script/minuteur.js
@@ -0,0 +1,97 @@
+document.addEventListener("DOMContentLoaded", function loaded() {
+  let numbersDiv = document.querySelector("#numbersTimer");
+  let heureInput = document.querySelector("#hours");
+  let minuteInput = document.querySelector("#minutes");
+  let secondsInput = document.querySelector("#seconds");
+  let popup = document.querySelectorAll(".popup");
+  popup = popup[0];
+  popup.style.display = "none";
+  let popup_p = document.querySelector("#popup-p");
+  let popupButtton = document.querySelector("#close-popup");
+
+  // bouton pour reset
+  let buttonReset = document.querySelector("#reset");
+
+  // variables utiles
+  let time;
+
+  // la fonction va limiter le nb d'input que l'on peut rentrer
+  function checkInputNb(typeInput) {
+    let ValueInput = typeInput.value;
+
+    if (ValueInput.length < 3) {
+      return true;
+    } else {
+      ValueInput = ValueInput.substring(0, ValueInput.length - 1);
+      typeInput.value = ValueInput;
+    }
+  }
+
+  function minuteur() {
+    heureInput.addEventListener("keyup", function () {
+      checkInputNb(heureInput);
+    });
+
+    minuteInput.addEventListener("keyup", function () {
+      checkInputNb(minuteInput);
+    });
+
+    secondsInput.addEventListener("keyup", function () {
+      checkInputNb(secondsInput);
+    });
+  }
+
+  let button = document.querySelector("#buttonMinuteur");
+
+  // fonction de décrémentation
+  function decreaseTime() {
+    if (time == 0) {
+      // audio + popup
+      let audio = new Audio("sounds/minuteur.mp3");
+      audio.play();
+      popup.style.display = "block";
+      popup_p.innerHTML = "Le temps est écoulé !";
+      popupButtton.addEventListener("click", function () {
+        audio.pause();
+        popup.style.display = "none";
+        reset();
+      });
+    } else {
+      time--;
+      let hours = Math.floor(time / 3600);
+      let minutes = Math.floor((time - hours * 3600) / 60);
+      let seconds = time - hours * 3600 - minutes * 60;
+      // console.log(hours, minutes, seconds);
+      numbersDiv.innerHTML = hours + ":" + minutes + ":" + seconds;
+    }
+  }
+
+  //  lancement du timer lors du click
+  button.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (button.dataset.switch == "on") {
+      button.innerHTML = "stop";
+      let heureValue = parseInt(heureInput.value);
+      let minValue = parseInt(minuteInput.value);
+      let secValue = parseInt(secondsInput.value);
+
+      time = heureValue * 3600 + minValue * 60 + secValue;
+      //   console.log(time, typeof time);
+      timesUp = setInterval(decreaseTime, 1000);
+      delete button.dataset.switch;
+    } else {
+      button.innerHTML = "start";
+      button.dataset.switch = "on";
+      clearInterval(timesUp);
+    }
+  });
+
+  // fonction pour reset
+  function reset() {
+    location.reload();
+  }
+
+  minuteur();
+
+  buttonReset.addEventListener("click", reset);
+});
