dbb9cf4af269bebf2dbd161b9b5697c978187fde
thomas-spinec <thomas.spinec@laplateforme.io>
2023-02-01 11:00:06 +0100

    début du projet oclock, index et un peu de css

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>O-clock</title>
+    <link rel="icon" type="images/png" sizes="64x64" href="img/Logo_onglet.png" />
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <header>
+        <section class="centrage" id="header">
+            <h1>O-clock</h1>
+            <p>L'horloge multifonction à portée de souris!</p>
+        </section>
+    </header>
+    <main>
+        <section class="centrage">
+            <div id="lien">
+                <a href="horloge.php"><button>Horloge</button></a>
+                <a href="reveil.php"><button>Réveil</button></a>
+                <a href="chronometre.php"><button>Chronomètre</button></a>
+                <a href="minuteur.php"><button>Minuteur</button></a>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <section class="centrage">
+            <img id="perso" src="img/Logo.png" alt="Thomas Spinec">
+        </section>
+    </footer>
+
+</body>
+
+</html>
